2624548b3caef8931325fabc7836b6e137428456
carlos falivene <carlosjfalivene@gmail.com>
2021-09-01 12:01:06 -0300

    Agregando imagenes a la pagina al hacer click en enviar, agregando links al local storage, creando archivo css del main y acomodando estructura htmldel main

--- js/scriptMain.js
+++ js/scriptMain.js
@@ -1,10 +1,28 @@
 
+let usuariosFotos = [];
+let imagen = document.getElementById('img').value;
+
 function envioFoto(){
     let imagen = document.getElementById('img').value;
     let img = new Image();
     img.src = imagen;
-    let foto = document.getElementById('espacioFoto');
-    espacioFoto.append(img );
+    let foto = document.getElementById('foto');
+    let nuevoUl = document.createElement('ul');
+    let nuevoLi = document.createElement('li');
+    nuevoLi.append(img)
+    nuevoUl.append(nuevoLi);
+    foto.append(nuevoUl );
+
+    usuariosFotos.push(imagen)
+
+    let bd = localStorage.setItem('FotoUsuario',JSON.stringify(usuariosFotos))
     
     
 }
+let array = JSON.parse(localStorage.getItem('FotoUsuario'))
+for(let i = 0; i < JSON.parse(localStorage.getItem('FotoUsuario')).length; i++){
+    console.log(array[i]);
+}
+
+
+   
